feb59ccc735ac741c1c8bbd3f52cb57ee8d6e9db
wooj-hub <wjddnwls0803@naver.com>
2024-06-22 09:08:01 +0900

    day03

--- reactstart/src/App.tsx
+++ reactstart/src/App.tsx
@@ -1,35 +1,5 @@
-import { FaFacebook, FaGoogle, FaApple } from "react-icons/fa";
-import SNSLogin from "./SNSLogin";
-
-// 스낵바
-import Snackbar from "./Snackbar";
-import { IoIosInformationCircle } from "react-icons/io";
-
 function App() {
-  return (
-    <div className="App">
-      <SNSLogin
-        backgroundColor="#1877F2"
-        snsText={{ sns: "Facebook" }}
-        snsIcon={{ Icon: FaFacebook }}
-      />
-      <SNSLogin
-        backgroundColor="black"
-        snsText={{ sns: "Apple" }}
-        snsIcon={{ Icon: FaApple }}
-      />
-      <SNSLogin
-        backgroundColor="white"
-        snsText={{ sns: "Google" }}
-        snsIcon={{ Icon: FaGoogle }}
-      />
-
-      {/* <Snackbar
-        messageText={{ message: "message" }}
-        statusIcon={{ color: "check", Icon: IoIosInformationCircle }}
-      /> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
