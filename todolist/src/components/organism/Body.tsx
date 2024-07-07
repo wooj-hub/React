3ab8412b75a382eaac9900c4ecea66a08dc002ab
wooj-hub <wjddnwls0803@naver.com>
2024-07-07 14:25:10 +0900

    day08

--- todolist/src/components/organism/Body.tsx
+++ todolist/src/components/organism/Body.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddButton from "../atom/AddButton";
 import TodoItem, { TodoItemProps } from "../molecule/TodoItem";
 import { userInput } from "../../utils/util";
@@ -8,9 +8,6 @@ const Body = () => {
   const [id, setId] = useState<number>(0);
   const [todoitems, setTodoitems] = useState<TodoItemProps[]>([
     // id를 미리 입력하면 새로 입력하는 거랑 충돌안됨???????
-    { id: 0, contents: "잠자기" },
-    { id: 1, contents: "축구" },
-    { id: 2, contents: "놀기" },
   ]);
 
   const addItem = () => {
@@ -21,19 +18,51 @@ const Body = () => {
     //코드 단순화. 위에랑 같은 의미
     const newData = { id, contents: userInput(INFO.TODO) };
     setTodoitems((prev) => {
+      //storage에 값들 저장하기. storage에 값이 저장되어야 새로고침했을 때 값들이 유지됨.
+      const updatedData = [...prev, newData];
+      // 배열은 storage에 못들어가서 문자화 시켜줘야 함 JSON.stringify
+      localStorage.setItem("TodoList", JSON.stringify(updatedData));
       return [...prev, newData];
     });
 
     setId((prev) => prev + 1);
-
-    //JSON.stringify(): obj ->string
-    localStorage.setItem(String(id), JSON.stringify(newData));
   };
+
+  //새로고침했을때 ???
+  useEffect(() => {
+    // storage에 저장된 값(문자) 밖으로 출력하기. null 값이면 "[]". 그냥 []이렇게 하면 문자가 아니라서 JSON.parse에서 오류남
+    const todolist = localStorage.getItem("TodoList") || "[]";
+    // 문자에서 배열로 바꾸기 =>JSON.parse(todolist)
+    const ArrayTodolist = JSON.parse(todolist);
+
+    //?????? 새로 고침했을 때 id를 다시 순서대로 정렬해줌
+    const mappedArray = ArrayTodolist.map((v: any, i: number) => ({
+      id: i,
+      contents: v.contents,
+    }));
+    ///??????새로 고침했을 때 id를 다시 순서대로 정렬해줌
+    localStorage.setItem("TodoList", JSON.stringify(mappedArray));
+    setTodoitems(mappedArray);
+    // . 했을때 length 안나와??? 타입이 명시적이지 않아서 그렇대. 사용은 가능
+    setId(ArrayTodolist.length);
+  }, []);
 
   const deleteItem = (id: number) => {
     // id가 삭제할 id와 같지 않은 경우에만 true를 반환합니다.
     // 따라서 true들만 유지 => 삭제할 거 삭제하고 나머지 요소만 유지
-    setTodoitems((prev) => prev.filter((value) => value.id !== id));
+    // setTodoitems((prev) => prev.filter((value) => value.id !== id));
+
+    setTodoitems((prev) => {
+      const updatedData = prev.filter((value) => value.id !== id);
+      //데이터를 삭제했을 때 id도 알맞게 줄여져야 함
+      //이걸 안하면 id 값은 줄어들지 않음 . 근데 delete 했을 때 에러가 있음.
+      //그래서 새로 고침했을 때 다시 정렬하도록 수정
+      // const mappedData = updatedData.map((v, i) => {
+      //   return { id: i, contents: v.contents };
+      // });
+      localStorage.setItem("TodoList", JSON.stringify(updatedData));
+      return updatedData;
+    });
   };
 
   return (
