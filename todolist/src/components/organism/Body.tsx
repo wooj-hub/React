55e7c1180fe56efe40c6484ee111ebb304dd46c9
wooj-hub <wjddnwls0803@naver.com>
2024-07-07 15:52:53 +0900

    day08

--- todolist/src/components/organism/Body.tsx
+++ todolist/src/components/organism/Body.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import AddButton from "../atom/AddButton";
 import TodoItem, { TodoItemProps } from "../molecule/TodoItem";
-import { localGet, localSet, userInput } from "../../utils/util";
+import {
+  inititializeTodoList,
+  localGet,
+  localSet,
+  userInput,
+} from "../../utils/util";
 import { INFO } from "../../constants/infoMessage";
 import { APIKEY } from "../../constants/apikey";
 
 //코드 간단화
-//1. 키값을 함수로 빼기 => apikey
-//2. localStorage.setItem(APIKEY.LOCAL, JSON.stringify(value)); 함수로 빼기 => util localSet
-//3. localStorage.getItem(APIKEY.LOCAL) || "[]"; 함스로 빼기 => util localGet
+//useEffect 코드 간단화 => util inititializeTodoList
 
 const Body = () => {
   const [id, setId] = useState<number>(0);
@@ -38,28 +41,8 @@ const Body = () => {
 
   //새로고침했을때 ???
   useEffect(() => {
-    // storage에 저장된 값(문자) 밖으로 출력하기. null 값이면 "[]". 그냥 []이렇게 하면 문자가 아니라서 JSON.parse에서 오류남
-    // const todolist = localStorage.getItem(APIKEY.LOCAL) || "[]"; 와 같은의미
-    const todolist = localGet();
-    // 문자에서 배열로 바꾸기 =>JSON.parse(todolist)
-    const ArrayTodolist = JSON.parse(todolist);
-
-    //위에 코드를 더 간단화
-    // const ArrayTodolist = JSON.parse(localGet());
-
-    //?????? 새로 고침했을 때 id를 다시 순서대로 정렬해줌
-    const mappedArray = ArrayTodolist.map((v: any, i: number) => ({
-      id: i,
-      contents: v.contents,
-    }));
-    ///??????새로 고침했을 때 id를 다시 순서대로 정렬해줌
-    // localStorage.setItem(APIKEY.LOCAL, JSON.stringify(mappedArray));
-    localSet(mappedArray);
-    //새로 고침했을 때 id를 다시 순서대로 정렬해줌. 근데 이것도 object 에러 떠서 storage에서 object 값 삭제후 다시 실행하기
-    //object에 대한 오류도 원래는 처리해줘야함
-    setTodoitems(mappedArray);
-    // . 했을때 length 안나와??? 타입이 명시적이지 않아서 그렇대. 사용은 가능
-    setId(ArrayTodolist.length);
+    //코드 간단화
+    inititializeTodoList({ setTodoitems, setId });
   }, []);
 
   const deleteItem = (id: number) => {
--- todolist/src/utils/util.ts
+++ todolist/src/utils/util.ts
@@ -1,3 +1,5 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { TodoItemProps } from "../components/molecule/TodoItem";
 import { APIKEY } from "../constants/apikey";
 import { ERROR } from "../constants/errorMessage";
 
@@ -8,3 +10,23 @@ export const localSet = (value: any) =>
   localStorage.setItem(APIKEY.LOCAL, JSON.stringify(value));
 
 export const localGet = () => localStorage.getItem(APIKEY.LOCAL) || "[]";
+
+type inititializeTodoListParams = {
+  //   setTodoList: React.Dispatch<React.SetStateAction<TodoItemProps[]>>;
+  //   setTodoId: React.Dispatch<React.SetStateAction<number>>;
+  setTodoitems: Dispatch<SetStateAction<TodoItemProps[]>>;
+  setId: Dispatch<SetStateAction<number>>;
+};
+
+export const inititializeTodoList = ({
+  setTodoitems,
+  setId,
+}: inititializeTodoListParams) => {
+  const mappedArray = JSON.parse(localGet()).map((v: any, i: number) => ({
+    id: i,
+    contents: v.contents,
+  }));
+  localSet(mappedArray);
+  setTodoitems(mappedArray);
+  setId(mappedArray.length);
+};
